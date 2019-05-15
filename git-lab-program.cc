4e7d9dd7ee6a4e851ceb7d9b75eff53547070327
Adam <moyera1@gmail.com>
2019-05-15 11:20:04 -0400

    added git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Adam Moyer
+ *        Date: 5/15/2019
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
